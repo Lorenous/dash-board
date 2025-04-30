4d34c4ac67ba118ba3e2fb01e7fe96ecadad4e63
Lorenous <osamaabaelziz@gmail.com>
2025-04-30 07:42:06 +0300

    create app images

---
+++ lib/utils/app_images.dart
@@ -0,0 +1,57 @@
+abstract class AppImages {
+  /// Assets for imagesCardReceive
+  /// assets/images/card-receive.svg
+  static const String cardReceive = "assets/images/card-receive.svg";
+
+  /// Assets for imagesCardSend
+  /// assets/images/card-send.svg
+  static const String cardSend = "assets/images/card-send.svg";
+
+  /// Assets for imagesCategory2
+  /// assets/images/category-2.svg
+  static const String category2 = "assets/images/category-2.svg";
+
+  /// Assets for imagesChart2
+  /// assets/images/chart-2.svg
+  static const String chart2 = "assets/images/chart-2.svg";
+
+  /// Assets for imagesConvertCard
+  /// assets/images/convert-card.svg
+  static const String convertCard = "assets/images/convert-card.svg";
+
+  /// Assets for imagesFrame
+  /// assets/images/Frame.svg
+  static const String frame = "assets/images/Frame.svg";
+
+  /// Assets for imagesFrame2
+  /// assets/images/Frame2.svg
+  static const String frame2 = "assets/images/Frame2.svg";
+
+  /// Assets for imagesGallery
+  /// assets/images/gallery.svg
+  static const String gallery = "assets/images/gallery.svg";
+
+  /// Assets for imagesGraph
+  /// assets/images/graph.svg
+  static const String graph = "assets/images/graph.svg";
+
+  /// Assets for imagesLogout
+  /// assets/images/logout.svg
+  static const String logout = "assets/images/logout.svg";
+
+  /// Assets for imagesMask group
+  /// assets/images/Mask group.svg
+  static const String maskGroup = "assets/images/Mask group.svg";
+
+  /// Assets for imagesMoneys
+  /// assets/images/moneys.svg
+  static const String moneys = "assets/images/moneys.svg";
+
+  /// Assets for imagesSetting2
+  /// assets/images/setting-2.svg
+  static const String setting2 = "assets/images/setting-2.svg";
+
+  /// Assets for imagesWallet2
+  /// assets/images/wallet-2.svg
+  static const String wallet2 = "assets/images/wallet-2.svg";
+}
