7630090239f10b7892c632c9160af447643f84a3
Lorenous <osamaabaelziz@gmail.com>
2025-05-28 08:49:44 +0300

    create my card

--- lib/widgets/dashboard_disktop_layout_widget.dart
+++ lib/widgets/dashboard_disktop_layout_widget.dart
@@ -1,5 +1,7 @@
 import 'package:dash_board/widgets/all_expenses_and_quick_invoice_section.dart';
+import 'package:dash_board/widgets/custom_background_container.dart';
 import 'package:dash_board/widgets/custom_drawer.dart';
+import 'package:dash_board/widgets/my_card.dart';
 import 'package:flutter/material.dart';
 
 class DashboardDisktopLayoutWidget extends StatelessWidget {
@@ -13,10 +15,28 @@ class DashboardDisktopLayoutWidget extends StatelessWidget {
           child: CustomDrawer(),
         ),
         Expanded(
-          flex: 2,
+          flex: 3,
           child: Padding(
             padding: EdgeInsets.symmetric(horizontal: 32, vertical: 42),
             child: AllExpensesAndQuickInvoiceSection(),
+          ),
+        ),
+        Expanded(
+          flex: 2,
+          child: Padding(
+            padding: EdgeInsets.only(top: 42, bottom: 42, right: 32),
+            child: Column(
+              children: [
+                CustomBackgroundContainer(
+                  padding: 24,
+                  child: Column(
+                    children: [
+                      MyCard(),
+                    ],
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ],
